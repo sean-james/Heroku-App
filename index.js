7888f64816469ebeb79346f5eaf75ef76121a1ef
Seán <seanjstone@outlook.com>
2020-09-21 01:45:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-class ApplicationController < ActionController::Base
-  before_filter :check_redirect_key
-
-  def check_redirect_key
-    if request[:access_key] != 'random_string' && cookies[:access_key] != 'random_string'
-      redirect_to "/404.html" 
-    elsif request[:access_key] == 'random_string'
-      cookies.permanent[:access_key] = 'random_string'
-    end
-  end
-
-end
-
 'use strict';
 
 // Load .env
